26fe6a66912f348b04386b2818679b59ace82cf5
王志龙 <991405736qq@gmail.com>
2015-08-06 14:52:45 +0800

    Initial Commit

---
+++ iOS:OS X WorkSpace/papapa/papapa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  papapa
+//
+//  Created by 王志龙 on 15/8/6.
+//  Copyright (c) 2015年 wangzhilong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
